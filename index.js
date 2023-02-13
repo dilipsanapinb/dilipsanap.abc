6401fb31e90c1383fdf28c11821edb344dc0907f
Dilip Sanap <112867608+dilipsanapinb@users.noreply.github.com>
2023-02-13 20:32:25 +0530

    6

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ menu.onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+
+//Github stats
+GitHubCalendar(".calendar", "dilipsanapinb");
+
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "dilipsanapinb", { responsive: true });
+
+    // Use a proxy
+    GitHubCalendar(".calendar", "dilipsanapinb", {
+       proxy (username) {
+         return fetch(`https://github.com/dilipsanapinb/dilipsanap.github.io/github?user=${username}`)
+       }
+    }).then(r => r.text())
